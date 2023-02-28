00bdeccf6d238b8f00e3a0f5e78988def6f6095c
SalvaGames <dmitrijus.doulaveris@gmail.com>
2023-02-28 19:44:05 +0200

    added pagination

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { Pagination } from '@mui/material/Pagination';
+import { Pagination } from '@mui/material';
 import { Box, Stack, Typography } from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
 import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({ exercises, bodyPart, setExercises }) => {
+  const[currentPage, setCurrentPage] = useState(1);
+  const exercisesPerPage = 9;
+
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
+
+  const paginate = (e/*e = event*/, value) => {
+    setCurrentPage(value);
+
+    window.scrollTo({top: 1800, behavior: 'smooth'})
+  }
+
   console.log(exercises)
   return (
     <Box id="exercises"
@@ -18,12 +32,25 @@ const Exercises = ({ exercises, bodyPart, setExercises }) => {
         Showing results
       </Typography>
       <Stack direction="row" sx={{gap: { lg: '110px', xs: '50px'}}}
-      flexWrap="warp"
+      flexWrap="wrap"
       justifyContent="center"
       >
-        {exercises.map((exercise, index) => (
+        {currentExercises.map((exercise, index) => (
           <ExerciseCard key={index} exercise={exercise}/>
         ))}
+      </Stack>
+      <Stack mt="100px" alignItems="center">
+          {exercises.length > 9 && (
+            <Pagination 
+              color="standard"
+              shape="rounded"
+              defaultPage={1}
+              count={Math.ceil(exercises.length / exercisesPerPage)}
+              page={currentPage}
+              onChange={paginate}
+              size="large"
+            />
+          )}
       </Stack>
     </Box>
   )
