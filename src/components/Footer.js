b9b3cda2e58eff6bef0c3035f9e92e3fb1ef0a3e
SalvaGames <dmitrijus.doulaveris@gmail.com>
2023-03-05 20:59:30 +0200

    app finished, added simillarExercises, loader, footer components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Box, Stack, Typography, Link  } from '@mui/material';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box mt="80px" bgcolor="#fff3f4">
+      <Stack gap="40px" allignItems="center" px="60px" pt="24px">
+      <Link href="https://github.com/Chris-55/gym-app" target="_blank" rel="noreferrer" > Click to visit github</Link>
+      </Stack>
+    </Box>
   )
 }
 
--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import HorizontalScrollBar from './HorizontalScrollBar';
+import Loader from './Loader';
 
-export const SimilarExercises = () => {
+export const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
   return (
-    <div>SimilarExercises</div>
+    <Box sx={{ mt: { lg: '100px', xs: '0px' }}}>
+      <Typography variant="h3" mb={5}>
+        Exercises that target the same muscle group
+      </Typography>
+      <Stack direction="row" styles={{ p: '2', position: 'relative'}}>
+        {targetMuscleExercises.length ? <HorizontalScrollBar data={targetMuscleExercises} />
+        : <Loader/>  
+        }
+      </Stack>
+      <Typography variant="h3" mb={5}>
+        Exercises that use the same equipment
+      </Typography>
+      <Stack direction="row" styles={{ p: '2', position: 'relative'}}>
+        {equipmentExercises.length ? <HorizontalScrollBar data={equipmentExercises} />
+        : <Loader/>  
+        }
+      </Stack>
+    </Box>
   )
 }
