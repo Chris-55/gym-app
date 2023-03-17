b26077776ab68e99d039afb893707efe6b73dc3a
SalvaGames <dmitrijus.doulaveris@gmail.com>
2023-03-17 12:09:59 +0200

    Revert "managed json file for github pages"
    This reverts commit ab0a5eff09c5eb17cf3150476fef3ec1cc2e95e7.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <Box mt="80px" bgcolor="#fff3f4">
       <Stack gap="40px" allignItems="center" px="60px" pt="24px">
-      <Link href="https://github.com/Chris-55/gymApp" target="_blank" rel="noreferrer" > Click to visit github</Link>
+      <Link href="https://github.com/Chris-55/gym-app" target="_blank" rel="noreferrer" > Click to visit github</Link>
       </Stack>
     </Box>
   )
